23e05fdf199febed32b0f378e6739e56ce03df6f
cosmoelton <net.elton@gmail.com>
2025-06-02 16:35:23 -0300

    Controller/users e authentication

---
+++ src/controllers/userController.js
@@ -0,0 +1,91 @@
+import supabese from "../config/supabase";
+import bcrypt from "bcrypt";
+import { response } from "express";
+import { request } from "http";
+import jwt from "jsonwebtoken";
+import supabase from "../config/supabase";
+import { error } from "console";
+
+//Cadastro de Usuário
+// async function registerUser (request, response,){
+const registerUser = async (request, response) => {
+
+//Desestruturação do Objeto
+const {nome, email, senhainformada} = request.boby;
+
+//cria um hash para senha informada
+const passwordHashed = await bcrypt.hast(senhainformada, 10);
+
+//Abre a conexão com o supabase (tabela Users)
+const {data, error} = await supabase.from('users').insert([
+
+{
+    nome,
+    email,
+    senhainformada: passwordHashed
+
+}
+
+]);
+
+if (error){
+    return response.status(500).json({
+    
+        erro: "Erro:", error
+
+    });
+
+}
+
+response.status(201).json({
+        mensagem: "Usuario cadastrado com sucesso!"
+
+    });
+
+};
+
+//login 
+const authentication = async (request.response) => {
+//Desestruturação do objeto
+
+const {email, senhainformada } = request.boby;
+
+//Abre a conexão com o supabase (tabela Users)
+const {data: user, error} = await supabase.from('users')
+
+.select("*")
+.eq("email", email)
+.single();
+
+if (error || !users) {
+
+    return response.status(401).json({
+        mensagem: "Credencias inválidas"
+    });
+}
+//Criptografa a senha e compara a senha que está no BD.
+const senhaEnviada = await bcrypt.compare(senhainformada, user.senha);
+
+if (!senhaEnviada){
+
+    return response.status(401).json({
+
+        mensagem: "Credencias inválidas"
+
+    });
+}
+
+//Gerar token
+const token = jwt.sign(
+    {id:user.id},
+    process.env.jwt.JWT_SECRET,
+    {expiresIn: "4h"}
+
+);
+
+response.json({token});
+
+};
+
+
+
