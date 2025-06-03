5007df5be682d224783cc801dc4a31c151767e5c
cosmoelton <net.elton@gmail.com>
2025-06-03 17:03:11 -0300

    Limpando arquivos da branch main

--- README.md
+++
@@ -1,2 +0,0 @@
-# full-project
-Projeto Completo Full-stack utilizando JAva Script , React, Node, Express, JWT e Bcrypt e Mysql
--- server.js
+++
@@ -1,9 +0,0 @@
-import app from "./src/app";
-
-const port = process.env.port || 3300;
-
-app.listen(port, () => {
-console.log("Servidor funcionando ");
-
-
-});
