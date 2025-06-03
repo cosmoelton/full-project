fc9ab1d4275af7128c32683b70383cc18eb1ae9f
cosmoelton <net.elton@gmail.com>
2025-06-03 15:39:34 -0300

    feat/user.routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,3 +1,4 @@
+import { Router } from "express";
 import express from express;
 import controller from "../controllers/userController";
 import auth from "authenticator";
@@ -6,3 +7,13 @@ import auth from "authenticator";
 const router = express.Router();
 
 router.get("/", auth, controller.listUsers);
+
+router.post("/register", controller.registerUser);
+
+router.post("/login", controller.authentication)
+
+router.put("/:id", auth, controller.updateUser);
+
+router.delete(":id", auth, controller.deleteUser);
+
+export default router;
